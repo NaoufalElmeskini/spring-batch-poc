728aa48b94e9cfe6cad80dbfe7028a093bce280c
Naoufal EL MESKINI <naoufal_el_meskini@carrefour.com>
2021-12-25 02:16:55 +0100

    initial commit

---
+++ src/test/java/io/nacrobate/springbatchpoc/SpringBatchPocApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nacrobate.springbatchpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
